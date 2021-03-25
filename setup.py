dc58dcd43cabc5efaf94a9c7fa48c52cd6da4f5d
Scott <slongwell@users.noreply.github.com>
2021-03-25 13:54:22 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+# with open('requirements.txt') as f:
+#     requirements = f.read().splitlines()
+
+setup(
+	name = 'seequence',
+	version = '0.1.0',
+	url = '',
+	author = 'Scott Longwell',
+	author_email = 'scott.a.longwell@gmail.com',
+	description = 'Visualize biosequences',
+	packages = find_packages(),    
+	install_requires = [],
+)
